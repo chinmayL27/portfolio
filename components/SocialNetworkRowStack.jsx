42658ce991b03b838509dbad1ce22d8c4462ac11
Chinmay Lohani <chinmaylohani27@gmail.com>
2024-01-06 22:39:28 -0800

    remove insta, whatsapp

--- components/SocialNetworkRowStack.jsx
+++ components/SocialNetworkRowStack.jsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
-import { Instagram } from "@styled-icons/icomoon/Instagram";
+// import { Instagram } from "@styled-icons/icomoon/Instagram";
 import { MailSend } from "@styled-icons/boxicons-regular/MailSend";
 import { LinkedinSquare } from "@styled-icons/boxicons-logos/LinkedinSquare";
 import { Github } from "@styled-icons/bootstrap/Github";
-import { Whatsapp } from "styled-icons/boxicons-logos";
+// import { Whatsapp } from "styled-icons/boxicons-logos";
 import { Kaggle } from "@styled-icons/simple-icons/Kaggle";
 
 const SocialMediaContainer = styled.div`
@@ -79,17 +79,17 @@ export default function SocialNetworkRowStack() {
 				<Kaggle />
 			</ButtonSocialMediaIcon>
 
-			<ButtonSocialMediaIcon href="(551)232-8278" target="_blank">
+			{/* <ButtonSocialMediaIcon href="(551)232-8278" target="_blank">
 				<Whatsapp />
-			</ButtonSocialMediaIcon>
+			</ButtonSocialMediaIcon> */}
 
 			<ButtonSocialMediaIcon href="mailto:chinmaylohani2711@gmail.com" target="_blank">
 				<MailSend />
 			</ButtonSocialMediaIcon>
 
-			<ButtonSocialMediaIcon href="https://www.instagram.com/im_chinmaylohani" target="_blank">
+			{/* <ButtonSocialMediaIcon href="https://www.instagram.com/im_chinmaylohani" target="_blank">
 				<Instagram />
-			</ButtonSocialMediaIcon>
+			</ButtonSocialMediaIcon> */}
 		</SocialMediaContainer>
 	);
 }
